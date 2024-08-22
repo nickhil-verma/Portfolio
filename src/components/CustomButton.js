4426d6a9e7e5e142a365ed1e2ed6e1eea05751d5
Nikhil Verma <vermanick75@gmail.com>
2024-08-23 01:21:02 +0530

    addded linkedin links

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,10 +1,9 @@
 // src/components/CustomButton.js
 
-import React from 'react';
-
-const CustomButton = ({ text }) => {
-  return (
-    <div className="relative w-48 h-12 border-2 m-auto my-5 dark:border-white border-black rounded-3xl overflow-hidden group cursor-pointer">
+const CustomButton = ({ text, className = '' }) => {
+    return (
+       
+    <div className="relative w-48 h-12 border-2 m-auto my-5 ${className} dark:border-white border-black rounded-3xl overflow-hidden group cursor-pointer">
       <div className="absolute inset-0 flex items-center justify-center   bg-yellow transform scale-0 group-hover:scale-100 m-0.5 transition-transform duration-700 rounded-3xl"></div>
       <div className="relative flex items-center justify-center h-full duration-700  dark:text-white hover:text-white text-black font-semibold z-10">
         {text}
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,6 +6,7 @@ import { motion } from 'framer-motion';
 import LOGO from '../img/CF_DP.png';
 import VEC1 from '../img/Vector1.png';
 import VEC2 from '../img/Vector2.png';
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 const Hero = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -54,59 +55,60 @@ const Hero = () => {
           <div className="relative text-center p-8">
             {/* Floating boxes positioned relative to the image */}
             
-
             <div className="relative z-20">
-  {/* Logo Image */}
-  <a href='https://www.instagram.com/nickhil_verma?igsh=MWljaDU2bW9kMmZ4dQ%3D%3D&utm_source=qr' className='cursor-pointer'><img
-    src={LOGO}
-    alt="Hero Image"
-    className="rounded-full hover:scale-105  duration-1000 cursor-pointer  z-40 w-[300px] -top-10 lg:w-[500px]"
-  /></a>
-  <motion.div
-              initial={{x:-110, opacity: 0 }}
-              whileInView={{ x:0, opacity: 1 }}
-              transition={{ delay: 2.8, duration: 0.8 }}
-              viewport={{ once: true }}
-              className="absolute lg:top-20 max-lg:top-10 lg:left-20 -top-16 -left- bg-white w-20 h-10 flex items-center justify-center text-xs rounded-lg shadow-lg z-40"
-            >
-              Web Developer
-            </motion.div>
+              {/* Logo Image */}
+              <a href='https://www.instagram.com/nickhil_verma?igsh=MWljaDU2bW9kMmZ4dQ%3D%3D&utm_source=qr' className='cursor-pointer'>
+                <img
+                  src={LOGO}
+                  alt="Hero Image"
+                  className="rounded-full hover:scale-105  duration-1000 cursor-pointer  z-40 w-[300px] -top-10 lg:w-[500px]"
+                />
+              </a>
+              <motion.div
+                initial={{x:-110, opacity: 0 }}
+                whileInView={{ x:0, opacity: 1 }}
+                transition={{ delay: 2.8, duration: 0.8 }}
+                viewport={{ once: true }}
+                className="absolute lg:top-20 max-lg:top-10 lg:left-20 -top-16 -left- bg-white w-20 h-10 flex items-center justify-center text-xs rounded-lg shadow-lg z-40"
+              >
+                Web Developer
+              </motion.div>
 
-            <motion.div
-              initial={{ x:100,opacity: 0 }}
-              whileInView={{ x:0, opacity: 1 }}
-              transition={{ delay: 2.8, duration: 0.8 }}
-              viewport={{ once: true }}
-              className="absolute max-sm:text-xs lg:bottom-16 max-lg:top-44 lg:-right-0 -bottom-10 -right-8 bg-white w-30 h-12 flex items-center justify-center text-xs p-2 rounded-lg shadow-lg z-40"
-            >
-              Competitive Programmer
-            </motion.div>
-  {/* VEC1 and VEC2 behind the logo */}
-  <img className='absolute -z-10 top-16 left-10' src={VEC1} />
-  <img className='absolute -z-10 top-20 left-10' src={VEC2} />
-  <motion.div className='bg-bluish top-20 right-0 z-0 blur-3xl w-48 h-48 absolute ' 
-  initial={{ x: '0',   opacity: 0.3 }}  // Initial position
-  animate={{ x: ['100px', '0px', '100px'], opacity: 0.5 }}  // To and fro in Y direction
-  transition={{
-    duration: 20, // Speed of the animation
-    ease: "easeInOut",  // Smooth easing function
-    repeat: Infinity,  // Loop indefinitely
-    repeatType: "mirror", // Reverses direction smoothly
-  }}
-  
-  />
-  <motion.div className='bg-yellow top-32 right-0 z-0 blur-3xl w-48 h-48 absolute ' 
-  initial={{ x: '0',   opacity: 0.5 }}  // Initial position
-  animate={{ x: ['-100px', '0px', '-100px'], opacity: 0.7 }}  // To and fro in Y direction
-  transition={{
-    duration: 20, // Speed of the animation
-    ease: "easeInOut",  // Smooth easing function
-    repeat: Infinity,  // Loop indefinitely
-    repeatType: "mirror", // Reverses direction smoothly
-  }}
-  
-  />
-</div>
+              <motion.div
+                initial={{ x:100,opacity: 0 }}
+                whileInView={{ x:0, opacity: 1 }}
+                transition={{ delay: 2.8, duration: 0.8 }}
+                viewport={{ once: true }}
+                className="absolute max-sm:text-xs lg:bottom-16 max-lg:top-44 lg:-right-0 -bottom-10 -right-8 bg-white w-30 h-12 flex items-center justify-center text-xs p-2 rounded-lg shadow-lg z-40"
+              >
+                Competitive Programmer
+              </motion.div>
+              {/* VEC1 and VEC2 behind the logo */}
+              <img className='absolute -z-10 top-16 left-10' src={VEC1} />
+              <img className='absolute -z-10 top-20 left-10' src={VEC2} />
+              <motion.div className='bg-bluish top-20 right-0 z-0 blur-3xl w-48 h-48 absolute ' 
+              initial={{ x: '0',   opacity: 0.3 }}  // Initial position
+              animate={{ x: ['100px', '0px', '100px'], opacity: 0.5 }}  // To and fro in Y direction
+              transition={{
+                duration: 20, // Speed of the animation
+                ease: "easeInOut",  // Smooth easing function
+                repeat: Infinity,  // Loop indefinitely
+                repeatType: "mirror", // Reverses direction smoothly
+              }}
+              
+              />
+              <motion.div className='bg-yellow top-32 right-0 z-0 blur-3xl w-48 h-48 absolute ' 
+              initial={{ x: '0',   opacity: 0.5 }}  // Initial position
+              animate={{ x: ['-100px', '0px', '-100px'], opacity: 0.7 }}  // To and fro in Y direction
+              transition={{
+                duration: 20, // Speed of the animation
+                ease: "easeInOut",  // Smooth easing function
+                repeat: Infinity,  // Loop indefinitely
+                repeatType: "mirror", // Reverses direction smoothly
+              }}
+              
+              />
+            </div>
 
           </div>
         </div>
@@ -114,7 +116,7 @@ const Hero = () => {
         {/* Left Side (Text) */}
         <div className="lg:w-2/5 w-full flex items-center justify-center bg-light dark:bg-dark duration-700">
           <div className="text-center p-8">
-            <h1 className="text-4xl max-sm:text-2xl duration-700 font-bold mb-4 dark:text-white dark:bg-dark">
+            <h1 className="text-4xl min-h-24 max-sm:text-2xl duration-700 font-bold mb-4 dark:text-white dark:bg-dark">
               HEY👋🏻{' '}
               <TypeAnimation
                 sequence={[
@@ -132,14 +134,19 @@ const Hero = () => {
               />
             </h1>
 
-            <p className="text-lg max-sm:text-sm text-gray-600 dark:text-gray-300 duration-700">
-              Discover our amazing services and features that can help you achieve your goals.
+            <p className="text-s max-sm:text-sm text-gray-600 dark:text-gray-300 duration-700">
+              Hey everyone! 👋 I'm a web developer looking for teammates to collaborate on hackathons. Let's build something amazing together! 💻🚀
             </p>
 
             {/* Custom Buttons */}
-            <a href='https://docs.google.com/document/d/1QjrcRxxFIcbXDU_ig183W7Jeud_3yybY3aeWLlu2x5I/edit?usp=sharing'>
-              <CustomButton text="Resume" />
-            </a>
+            <div className='flex items-center justify-center space-x-4 max-sm:flex-col max-sm:space-y-4'>
+  <a href='https://docs.google.com/document/d/1QjrcRxxFIcbXDU_ig183W7Jeud_3yybY3aeWLlu2x5I/edit?usp=sharing' className='flex justify-center'>
+    <CustomButton text="Resume" className="max-sm:w-16 max-sm:h-10" />
+  </a>
+  <a href="https://www.linkedin.com/in/nikhil-verma-b9ba861b0/" className='hover:scale-105 duration-1000 hover:text-yellow cursor-pointer' ><span className='dark:text-white max-sm:text-xs flex justify-center items-center '>Connect with me &nbsp; <FaExternalLinkAlt /></span></a>
+</div>
+
+
           </div>
         </div>
 
